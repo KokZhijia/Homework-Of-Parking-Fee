cc812ba5b42488300c7736f1646c3a8356e790b7
student <student@201-31>
2023-10-02 12:00:12 +0800

    加入專案檔案。

---
+++ Homework Of Parking Fee/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Homework_Of_Parking_Fee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Parkingtime = int.Parse(Console.ReadLine());
+            int Parkingfee = (Parkingtime / 30);
+            int charges = (Parkingfee *20 );
+            if (Parkingtime>=30)
+            {
+
+                Console.WriteLine($"NT$: {charges}");
+
+            }
+            else
+            {
+
+                Console.WriteLine("Free");
+
+            }
+            
+
+        }
+    }
+}
